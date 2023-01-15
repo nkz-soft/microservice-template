2861f63789b0cd65d5b245917f8c0573c1e17d3a
nkz <nczsoft@gmail.com>
2023-01-15 13:55:46 +0300

    Delete tests/NKZSoft.Template.Presentation.GRPC.Tests directory

--- tests/NKZSoft.Template.Presentation.GRPC.Tests/Common/GrpcCollectionDefinition.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NKZSoft.Template.Presentation.Grpc.Tests.Common;
-
-[CollectionDefinition(nameof(GrpcCollectionDefinition))]
-public sealed class GrpcCollectionDefinition : CoreCollectionDefinition<GrpcWebApplicationFactory<Program>>
-{
-}
