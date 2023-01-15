6d825c0397a5ea13136d48d36fe8a611a960a1f0
nkz <nczsoft@gmail.com>
2023-01-15 13:53:31 +0300

    Delete src/NKZSoft.Template.Presentation.GRPC directory

--- src/NKZSoft.Template.Presentation.GRPC/Models/GetTodoItemRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NKZSoft.Template.Presentation.Grpc.Models;
-
-[ProtoContract]
-public sealed record GetTodoItemRequest
-{
-    [ProtoMember(1)]
-    public Guid Id { get; init; }
-}
--- src/NKZSoft.Template.Presentation.GRPC/Models/GetTodoItemsRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NKZSoft.Template.Presentation.Grpc.Models;
-
-[ProtoContract]
-public sealed record GetTodoItemsRequest : PageContext
-{
-    [ProtoMember(1)]
-    public Guid[] Ids { get; init; } = Array.Empty<Guid>();
-}
