27e44a2d02c14a75f2e8b2a7d989d4675427b77f
nkz <5499916+nkz-soft@users.noreply.github.com>
2023-03-17 09:25:55 +0300

    fix tests

--- tests/NKZSoft.Template.Application.Tests/Startup.cs
+++ tests/NKZSoft.Template.Application.Tests/Startup.cs
@@ -8,7 +8,7 @@ using Application.Common.Repositories.PostgreSql;
 
 internal sealed class Startup
 {
-    public static async Task ConfigureServices(IServiceCollection services)
+    public static async void ConfigureServices(IServiceCollection services)
     {
         services.AddApplication();
         services.TryAddSingleton(AppMockFactory.CreateCurrentUserServiceMock());
